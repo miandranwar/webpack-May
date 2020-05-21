879dd204028a188edb318d56598c23c8c74caf50
Muhammad <miandranwarg@gmail.com>
2020-05-21 05:14:54 +0100

    New plugin and change in pug ans css

--- config.prod.js
+++ config.prod.js
@@ -1,17 +1,20 @@
 let path = require('path');
 let htmlplugin = require('html-webpack-plugin');
 let minicss = require('mini-css-extract-plugin');
+let manifest = require('webpack-manifest-plugin');
 
 
 module.exports = {
     mode: 'production',
     plugins: [
+        new manifest(),
         new htmlplugin({ template: './src/views/index.pug' }),
         new minicss()
+        
     ],
     module: {
         rules: [
-            { test: /\.css$/i, use: [minicss.loader, 'css-loader', 'sass-loader'] },
+            { test: /\.s[ca]ss$/i, use: [minicss.loader, 'css-loader', 'sass-loader'] },
             { test: /\.pug$/i, use: ['pug-loader'] },
             { test: /\.(jpg|png|svg|gif)$/i, use: [{ loader: 'file-loader', options:{name:'[name].[ext]', outputpath: 'Asserts/images/', publicpath: 'Asserts/images/'} }] }
 
