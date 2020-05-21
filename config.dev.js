e28af8cc8d7590bab8380157f84259a2b50e57a4
Muhammad <miandranwarg@gmail.com>
2020-05-21 05:41:52 +0100

    Added Hot module in dev file

--- config.dev.js
+++ config.dev.js
@@ -23,6 +23,7 @@ module.exports = {
         
     devServer: {
         contentBase: path.join(__dirname, 'dist'),
+        hot: true,
         port: 3000
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
 import './styles/style.scss';
 import './images/hero.jpg';
+
+
+if (module.hot) {
+    module.hot.accept()
+};
